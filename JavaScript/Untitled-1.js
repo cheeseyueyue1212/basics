ad9bb3cdf504a6ded5770b616840d7a7ee799efb
Cheese <15510662303@163.com>
2020-07-01 15:40:07 +0800

    js小问题

--- JavaScript/Untitled-1.js
+++ JavaScript/Untitled-1.js
@@ -1,7 +1,10 @@
+var foo = 1;
 
+function bar() {
+    foo = 10;
+    return;
 
-
-var test = new Set([1,2,4,3])
-var test2 = new Set([2,5,6])
-
-var res = new Set([...test].filter((item, i) => {return [...test2].includes(item)}))
+    function foo() {}
+}
+bar();
+alert(foo);
